1540e24d9c7e0feb867cd5e18ca5c430006552ba
dbahiense <douglas@rohling.com.br>
2024-07-14 12:54:03 -0300

    navbar and sidebar layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
-import { Route, Routes } from "react-router-dom"; 
-import { Root, Toolbar, Content } from "./styles";
-import { Actors, Movies, MovieInformation, NavBar, Profile } from "./"
+import { SideBar } from "./"
 
 const App = () => {
   return (
-    <Root>
+    <>
       <CssBaseline />
-      <NavBar />
-      <Content>
-        <Toolbar />
-        <Routes>
-          <Route path="/" element={<Movies />} />
-          <Route path="/movies/:id" element={<MovieInformation />} />
-          <Route path="/actors/:id" element={<Actors />} />
-          <Route path="/profile/:id" element={<Profile />} />
-        </Routes>
-      </Content>
-    </Root>
+      <SideBar />
+    </>
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Brightness4 from "@mui/icons-material/Brightness4";
+import Brightness7 from "@mui/icons-material/Brightness7";
+import Button from "@mui/material/Button";
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from "@mui/material/Tooltip";
+import Typography from '@mui/material/Typography';
 
-const NavBar = () => {
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const isAuth = false;
+
   return (
-    <div>NavBar</div>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            aria-label="menu"
+            color="inherit"
+            edge="start"
+            size="large"
+            sx={{
+              mr: 2,
+              display: {
+                md: "none",
+              },
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-export default NavBar
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Video Hub
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+
+          <IconButton
+            color="inherit"
+            sx={{ ml: 2 }}  
+          >
+            <Brightness7 />
+          </IconButton>
+
+          <div>
+            {!isAuth ? (
+              <Button color="inherit">Login</Button>
+
+            ) : (
+              <Tooltip title="Open settings">
+                <IconButton sx={{ p: 0 }}>
+                  <Avatar alt="Douglas Bahiense" src="/" />
+                </IconButton>
+              </Tooltip>
+            )}
+          </div>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
